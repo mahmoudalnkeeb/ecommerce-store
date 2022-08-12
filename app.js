94b3fc20dcef508c90de0f3088ee937dd4ed36c1
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-08-12 18:53:07 +0200

    cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const app = express();
 
 // import routers
@@ -19,7 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 // routers
 
 app.use(indexRouter);
